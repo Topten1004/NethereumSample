0b754350c918e5b9856179568cfd855cba362a03
SeniorNet <97684719+arthurchen330@users.noreply.github.com>
2022-09-20 09:45:42 -0400

    Initial version

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using Nethereum.Web3;
+
+namespace NethereumSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAccountBalance().Wait();
+            Console.ReadLine();
+        }
+
+        static async Task GetAccountBalance()
+        {
+            var web3 = new Web3("https://mainnet.infura.io/v3/e27e450636c94111a358c80d3997ee9d");
+            var balance = await web3.Eth.GetBalance.SendRequestAsync("0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae");
+            Console.WriteLine($"Balance in Wei: {balance.Value}");
+
+            var etherAmount = Web3.Convert.FromWei(balance.Value);
+            Console.WriteLine($"Balance in Ether: {etherAmount}");
+        }
+    }
+}
